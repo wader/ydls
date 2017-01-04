0e3efd1f77acda80b1ee749a70c58956720e1db9
Mattias Wadman <mattias.wadman@gmail.com>
2017-01-04 23:02:45 +0100

    Add id3v2 test

---
+++ id3v2/id3v2_test.go
@@ -0,0 +1,35 @@
+package id3v2
+
+import (
+	"bytes"
+	"reflect"
+	"testing"
+)
+
+func TestWrite(t *testing.T) {
+	frames := []Frame{
+		&TextFrame{ID: "TIT2", Text: "title"},
+		&COMMFrame{Language: "lan", Description: "", Text: "description"},
+		&APICFrame{
+			MIMEType:    "image/test",
+			PictureType: PictureTypeOther,
+			Description: "description",
+			Data:        []byte{1, 2, 3},
+		},
+	}
+
+	actual := &bytes.Buffer{}
+	Write(actual, frames)
+
+	expected := []byte(
+		"ID3\x03\x00\x00\x00\x00\x00[" +
+			"TIT2\x00\x00\x00\a\x00\x00\x03title\x00" +
+			"COMM\x00\x00\x00\x10\x00\x00\x03lan\x00description" +
+			"APIC\x00\x00\x00\x1c\x00\x00\x03image/test\x00\x00description\x00\x01\x02\x03" +
+			"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00",
+	)
+
+	if !reflect.DeepEqual(actual.Bytes(), expected) {
+		t.Errorf("expected '%#v' actual '%#v'", string(expected), string(actual.Bytes()))
+	}
+}
