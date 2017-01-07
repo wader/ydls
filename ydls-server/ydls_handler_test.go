29058df74269dafa02313734fb4c38692a499684
Mattias Wadman <mattias.wadman@gmail.com>
2017-01-07 17:59:59 +0100

    Add more TestYDLSHandler tests

--- ydls-server/ydls_handler_test.go
+++ ydls-server/ydls_handler_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"html/template"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -104,7 +105,7 @@ func ydlsHandlerFromFormatsEnv(t *testing.T) *ydlsHandler {
 	return yh
 }
 
-func TestYDLSHandler(t *testing.T) {
+func TestYDLSHandlerDownload(t *testing.T) {
 	if !testNetwork || !testFfmpeg || !testYoutubeldl {
 		t.SkipNow()
 	}
@@ -136,5 +137,58 @@ func TestYDLSHandler(t *testing.T) {
 	}
 	if res.Header.Get("Content-Type") == "mpeg/audio" {
 		t.Errorf("expected a Content-Type to be mpeg/audio, got %s", res.Header.Get("Content-Type"))
+	}
+}
+
+func TestYDLSHandlerBadURL(t *testing.T) {
+	if !testNetwork || !testFfmpeg || !testYoutubeldl {
+		t.SkipNow()
+	}
+
+	ts := httptest.NewServer(ydlsHandlerFromFormatsEnv(t))
+	defer ts.Close()
+
+	testServerURL, _ := url.Parse(ts.URL)
+	ydlsTestURL := testServerURL.ResolveReference(&url.URL{
+		Path: "/badurl",
+	})
+
+	res, err := http.Get(ydlsTestURL.String())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if res.StatusCode != http.StatusBadRequest {
+		t.Errorf("expected bad request, got %d", res.StatusCode)
+	}
+}
+
+func TestYDLSHandlerIndexTemplate(t *testing.T) {
+	if !testNetwork || !testFfmpeg || !testYoutubeldl {
+		t.SkipNow()
+	}
+
+	yh := ydlsHandlerFromFormatsEnv(t)
+	yh.indexTmpl, _ = template.New("index").Parse("hello")
+	ts := httptest.NewServer(yh)
+	defer ts.Close()
+
+	testServerURL, _ := url.Parse(ts.URL)
+	res, err := http.Get(testServerURL.String())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("expected ok, got %d", res.StatusCode)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if string(body) != "hello" {
+		t.Errorf("expected hello, got %s", string(body))
 	}
 }
