4595dc900b0d7ddcaa715187f3ab2440873e6b1c
Mattias Wadman <mattias.wadman@gmail.com>
2017-03-22 23:50:52 +0100

    Add ydls-get/server raw test

--- test_cmd/ydls-get.sh
+++ test_cmd/ydls-get.sh
@@ -6,4 +6,7 @@ trap "rm -rf $TEMPDIR" EXIT
 cd "$TEMPDIR"
 
 ydls-get -formats "$FORMATS" "https://www.youtube.com/watch?v=C0DPdy98e4c" mp3
-ffprobe -hide_banner -i "TEST VIDEO.mp3" 2>&1 | grep mp3
+ffprobe -show_format -hide_banner -i "TEST VIDEO.mp3" 2>&1 | grep format_name=mp3
+
+ydls-get -formats "$FORMATS" "https://www.youtube.com/watch?v=C0DPdy98e4c"
+ffprobe -show_format -hide_banner -i "TEST VIDEO.webm" 2>&1 | grep format_name=matroska,webm
--- test_cmd/ydls-server.sh
+++ test_cmd/ydls-server.sh
@@ -7,6 +7,11 @@ cd "$TEMPDIR"
 
 ydls-server -listen :1234 -formats "$FORMATS" &
 sleep 1
+
 curl -OJ "http://0:1234/mp3/https://www.youtube.com/watch?v=C0DPdy98e4c"
-ffprobe -hide_banner -i "TEST VIDEO.mp3" 2>&1 | grep mp3
+ffprobe -show_format -hide_banner -i "TEST VIDEO.mp3" 2>&1 | grep format_name=mp3
+
+curl -OJ "http://0:1234/https://www.youtube.com/watch?v=C0DPdy98e4c"
+ffprobe -show_format -hide_banner -i "TEST VIDEO.webm" 2>&1 | grep format_name=matroska,webm
+
 kill %1
