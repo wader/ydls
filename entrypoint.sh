f6d7b33b629731e13268aaf0e2aaa158281f8017
Mattias Wadman <mattias.wadman@gmail.com>
2017-05-13 17:46:39 +0200

    Use  for get mode also

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 if [ "$1" = "" ] ; then
   exec tini -- ydls-server -info -formats "$FORMATS" -listen "$LISTEN"
 else
-  exec ydls-get -formats /etc/formats.json "$@"
+  exec ydls-get -formats "$FORMATS" "$@"
 fi
