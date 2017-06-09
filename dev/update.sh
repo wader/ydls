d14af08119749829d25299af0d09840fe1c9d235
Mattias Wadman <mattias.wadman@gmail.com>
2017-06-09 22:49:56 +0200

    Improve update script
    Fix version sort bug
    Add periodic check
    Add comment about how to run

--- dev/update.sh
+++ dev/update.sh
@@ -1,17 +1,30 @@
-FFMPEG_LATEST=$(git ls-remote -t https://github.com/FFmpeg/FFmpeg | cut -f 2 | grep '^refs/tags/n' | grep -v -- -dev | grep -v {} | sort -nr | head -n 1 | cut -d '/' -f 3)
-YDL_LATEST=$(git ls-remote -t https://github.com/rg3/youtube-dl/ | cut -f 2 | grep -v {} | sort -nr | head -n 1 | cut -d '/' -f 3)
+#!/bin/bash
 
-echo ffmpeg $FFMPEG_LATEST
-echo youtube-dl $YDL_LATEST
+# this scripts is used to auto update youtube-dl and ffmpeg versions
+# run with GIT_SSH_COMMAND="ssh -i sshkey-ydls" ./update.sh
 
-sed -i '' "s/FFMPEG_VERSION=.*/FFMPEG_VERSION=$FFMPEG_LATEST/g" Dockerfile
-if ! git diff --quiet Dockerfile ; then
-  git add Dockerfile
-  git commit -m "Update ffmpeg to $FFMPEG_LATEST"
-fi
+while true ; do
+  git pull
 
-sed -i '' "s/YDL_VERSION=.*/YDL_VERSION=$YDL_LATEST/g" Dockerfile
-if ! git diff --quiet Dockerfile ; then
-  git add Dockerfile
-  git commit -m "Update youtube-dl to $YDL_LATEST"
-fi
+  FFMPEG_LATEST=$(git ls-remote -t https://github.com/FFmpeg/FFmpeg | cut -f 2 | grep '^refs/tags/n' | grep -v -- -dev | grep -v {} | cut -d '/' -f 3 | sort -t. -k 1.2,1nr -k 2,2nr -k 3,3nr -k 4,4nr | head -n 1)
+  YDL_LATEST=$(git ls-remote -t https://github.com/rg3/youtube-dl/ | cut -f 2 | grep -v {} | sort -nr | head -n 1 | cut -d '/' -f 3)
+
+  echo ffmpeg $FFMPEG_LATEST
+  echo youtube-dl $YDL_LATEST
+
+  sed -i '' "s/FFMPEG_VERSION=.*/FFMPEG_VERSION=$FFMPEG_LATEST/g" Dockerfile
+  if ! git diff --quiet Dockerfile ; then
+    git add Dockerfile
+    git commit -m "Update ffmpeg to $FFMPEG_LATEST"
+  fi
+
+  sed -i '' "s/YDL_VERSION=.*/YDL_VERSION=$YDL_LATEST/g" Dockerfile
+  if ! git diff --quiet Dockerfile ; then
+    git add Dockerfile
+    git commit -m "Update youtube-dl to $YDL_LATEST"
+  fi
+
+  git push origin master
+
+  sleep 86400 # sleep for a day
+done
