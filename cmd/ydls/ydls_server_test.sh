73ee6679f8ef1ed22c98dbe84004cce78ce571d9
Mattias Wadman <mattias.wadman@gmail.com>
2019-03-01 01:18:04 +0100

    Silent curl in cmd test

--- cmd/ydls/ydls_server_test.sh
+++ cmd/ydls/ydls_server_test.sh
@@ -8,10 +8,10 @@ cd "$TEMPDIR"
 ydls -server -listen :1234 -config "$CONFIG" &
 sleep 1
 
-curl -OJ "http://0:1234/mp3+1s/https://www.youtube.com/watch?v=C0DPdy98e4c"
+curl -sOJ "http://0:1234/mp3+1s/https://www.youtube.com/watch?v=C0DPdy98e4c"
 ffprobe -show_format -hide_banner -i "TEST VIDEO.mp3" 2>&1 | grep format_name=mp3
 
-curl -OJ "http://0:1234/https://www.youtube.com/watch?v=C0DPdy98e4c"
+curl -sOJ "http://0:1234/https://www.youtube.com/watch?v=C0DPdy98e4c"
 ffprobe -show_format -hide_banner -i "TEST VIDEO.mp4" 2>&1 | grep format_name=mov
 
 kill %1
